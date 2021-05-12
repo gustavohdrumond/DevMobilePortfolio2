a5ca81b3921b455712dab64d0a4b57a2c6ae89ce
Gustavo Henrique Drumond Leão <35820435+gustavohdrumond@users.noreply.github.com>
2021-05-11 22:07:58 -0300

    Criação das páginas

---
+++ src/pages/prato-entrada/prato-entrada.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PratoEntradaPage } from './prato-entrada';
+
+@NgModule({
+  declarations: [
+    PratoEntradaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PratoEntradaPage),
+  ],
+})
+export class PratoEntradaPageModule {}
---
+++ src/pages/prato-refeicao/prato-refeicao.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PratoRefeicaoPage } from './prato-refeicao';
+
+@NgModule({
+  declarations: [
+    PratoRefeicaoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PratoRefeicaoPage),
+  ],
+})
+export class PratoRefeicaoPageModule {}
